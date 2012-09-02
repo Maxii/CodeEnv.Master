91017606020857cb2eb82cf1bb1fa316c68d78d5
Maxii <jim@strategicforge.com>
2012-09-02 08:08:45 -0400

    Added TrialUnityLibrary Project.

---
+++ CodeEnv.Master.TrialUnityLibrary/TrialMonoBehaviorClass.cs
@@ -0,0 +1,42 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright>
+// Copyright © 2012 Strategic Forge
+//
+// Email: jim@strategicforge.com
+// </copyright> 
+// <summary> 
+// File: TrialMonoBehaviorClass.cs
+// TODO - one line to give a brief idea of what this file does.
+// </summary> 
+// -------------------------------------------------------------------------------------------------------------------- 
+
+namespace CodeEnv.Master.TrialUnityLibrary {
+
+    using System;
+    using UnityEngine;
+    using UnityEditor;
+
+    /// <summary>
+    /// TODO 
+    /// </summary>
+    public class TrialMonoBehaviorClass : MonoBehaviour {
+
+        private void Awake() {
+
+        }
+
+        private void Start() {
+
+        }
+
+        private void Update() {
+
+        }
+
+        private void LateUpdate() {
+
+        }
+
+    }
+}
+
