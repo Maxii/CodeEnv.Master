6add5fc983ea6d6e8c85dcaf9026395656fe147c
Maxii <jim@strategicforge.com>
2015-02-09 07:12:48 -0500

    Prior to starting Publisher branch.

---
+++ CodeEnv.Master.GameContent/IPublisherItem.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright>
+// Copyright © 2012 - 2015 Strategic Forge
+//
+// Email: jim@strategicforge.com
+// </copyright> 
+// <summary> 
+// File: IPublisherItem.cs
+// COMMENT - one line to give a brief idea of what the file does.
+// </summary> 
+// -------------------------------------------------------------------------------------------------------------------- 
+
+#define DEBUG_LOG
+#define DEBUG_WARN
+#define DEBUG_ERROR
+
+namespace CodeEnv.Master.GameContent {
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IPublisherItem<DataType> where DataType : AItemData {
+
+        DataType Data { get; }
+
+    }
+}
+
